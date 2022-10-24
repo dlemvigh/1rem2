b31cfd927f40bb688a4259f27102bd5e941808ab
David Lemvigh <dle@siteimprove.com>
2022-10-25 00:10:12 +0200

    Fix Lighthouse SEO errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,9 @@
   import Calculator from "../components/calculator.svelte";
 </script>
 
+<svelte:head>
+  <title>1RM calculator</title>
+  <meta name="description" content="1-rep-maximum calculator.">
+</svelte:head>
+
 <Calculator />
